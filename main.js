1eabb0df75aedad2d33921ff5bcc66b00ad70fb3
정지원 <4dakto@gmail.com>
2015-11-19 17:48:31 +0900

    martial ui by angularjs

---
+++ main.js
@@ -0,0 +1,15 @@
+(function() {
+    "use strict";
+
+    var app = angular.module('StarterApp', ['ngMaterial']);
+
+    app.controller('AppController', function($mdSidenav) {
+        var vm = this;
+
+        vm.toggleSidenav = function(menuId) {
+            $mdSidenav(menuId).toggle();
+        };
+
+    });
+}());
+
